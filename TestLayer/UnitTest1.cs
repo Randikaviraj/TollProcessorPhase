17e168398821978da9e44327aa54a6f630a5c4d8
Randikaviraj <56146699+Randikaviraj@users.noreply.github.com>
2021-03-10 01:42:07 +0530

    Residental Test Ceases added

--- TestLayer/UnitTest1.cs
+++ TestLayer/UnitTest1.cs
@@ -25,5 +25,38 @@ namespace TestLayer
             Assert.Equal("2 axles", axle);
             
         }
+        [Fact]
+        public void TestDisplayMenu()
+        {
+            Dictionary<string, double> dic = new Dictionary<string, double>()
+                {
+                {"2 axles" ,2.50},
+                {"3 axles" ,3.50},
+                {"4 axles" ,4.50},
+                {"5 axles" ,5.00},
+                {"7 axles" ,7.00}
+            };
+            CommercialTollProcessor commercialTollProcessor = new CommercialTollProcessor(dic);
+
+            Assert.Equal<Dictionary<string, double>>(dic, commercialTollProcessor.DisplayMenu());
+
+        }
+
+
+        [Fact]
+        public void TestResidentalCalculateToll()
+        {
+            CommercialTollProcessor commercialTollProcessor = new CommercialTollProcessor(new Dictionary<string, double>()
+                {
+                {"2 axles" ,2.00},
+                {"3 axles" ,3.00},
+                {"4 axles" ,4.00}
+            });
+            double cost;
+            string axle;
+            (cost, axle) = commercialTollProcessor.CalculateToll(1);
+            Assert.Equal(2.00, cost);
+            Assert.Equal("2 axles", axle);
+        }
     }
 }
